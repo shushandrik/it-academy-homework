e7941cac2f18046a03149490cff464b01ac18db8
Andrei Shushkevich <sdaemby@gmail.com>
2023-01-26 15:09:10 +0300

    added node.js

--- lesson04/node.js
+++ lesson04/node.js
@@ -1,4 +1,13 @@
-const fs = require(fs-extra);
+const fs = require('fs-extra');
 
-
-
+fs.mkdirpSync ('Folder1');
+fs.createFileSync ('Folder1/newFile.txt');
+fs.mkdirpSync ('Folder2');
+fs.moveSync ('Folder1/newFile.txt', 'Folder2/newFile.txt');
+fs.mkdirpSync ('Folder3');
+fs.copyFileSync ('Folder2/newFile.txt', 'Folder3/newFile.txt');
+fs.removeSync('Folder2/newFile.txt');
+fs.removeSync('Folder3/newFile.txt');
+fs.removeSync('Folder1');
+fs.removeSync('Folder2');
+fs.removeSync('Folder3');

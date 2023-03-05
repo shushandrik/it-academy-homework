919e1d8e8636ea48c9102cd7138ab10ab383441e
Andrei Shushkevich <sdaemby@gmail.com>
2023-03-05 20:29:29 +0300

    MochaUnitTest

--- lesson14/Mocha/test/test.js
+++ lesson14/Mocha/test/test.js
@@ -2,9 +2,14 @@ const {expect} = require('chai');
 const Calculator = require ('../../calculator')
 
 describe ('Addition function test', function () {
+   const calculator = new Calculator();
     it ('Addition of 2 integers', async () => {
-        const functionResult = divide (10,10);
-        expect (functionResult).to.equal(20)
+        let addResult = calculator.add (10,10);
+        expect (addResult).to.equal(20)
+    } )
+    it ('Addition of 4 integers', async () => {
+        let addResult = calculator.add (10,10,10,20);
+        expect (addResult).to.equal(50)
     } )
 })
 
@@ -17,7 +22,7 @@ const {returnTrue} = require ('../../func')
 describe ('First test', function (){
     it ('Title that will be included in the report', async () => {
         const functionResult = returnTrue ();
-        expect (functionResult).to.equal(true)
+        expect (functionResult).to.equal(false)
     } )
 })
 */

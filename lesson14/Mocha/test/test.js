3fa66d4eb3545d65ec0cdc50c4f59acce1c73a75
Andrei Shushkevich <sdaemby@gmail.com>
2023-03-07 21:17:28 +0300

    MochaUnitTest

--- lesson14/Mocha/test/test.js
+++ lesson14/Mocha/test/test.js
@@ -4,14 +4,56 @@ const Calculator = require ('../../calculator')
 describe ('Addition function test', function () {
    const calculator = new Calculator();
     it ('Addition of 2 integers', async () => {
-        let addResult = calculator.add (10,10);
-        expect (addResult).to.equal(20)
+        let addResult = calculator.add (1,10);
+        expect (addResult).to.equal(11)
     } )
-    it ('Addition of 4 integers', async () => {
-        let addResult = calculator.add (10,10,10,20);
-        expect (addResult).to.equal(50)
+    it ('Addition of array (10) integers', async () => {
+        let addResult = calculator.add (10,20,30,40,50,60,70,80,90,100);
+        expect (addResult).to.equal(550)
+    } )
+    it ('Addition of 2 decimal number', async () => {
+        let addResult = calculator.add (55.5,40.7);
+        expect (addResult).to.equal(96.2)
+    } )
+    it ('Addition of negative integers', async () => {
+        let addResult = calculator.add (-45,15);
+        expect (addResult).to.equal(-30)
+    } )
+    it ('Addition of number and string', async () => {
+        let addResult = calculator.add (1,'1');
+        expect (addResult).to.equal(2)                   // узнать как прописать ошибку
+    } )
+    it ('Addition 17-digit number', async () => {
+        let addResult = calculator.add (1245874258742369,1536974568725468);
+        expect (addResult).to.equal(2782848827467837)                   
     } )
 })
+
+describe ('Multiply function test', function () {
+    const calculator = new Calculator();
+     it ('Multiply of 2 integers', async () => {
+         let addResult = calculator.multiply (1,10);
+         expect (addResult).to.equal(10)
+     } )
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

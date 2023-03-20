689772a608bbfc42b70d69d9d6b179206a8119ea
Andrei Shushkevich <sdaemby@gmail.com>
2023-03-20 15:20:09 +0300

    finished with tests

--- lesson17/test/test.js
+++ lesson17/test/test.js
@@ -1,11 +1,12 @@
 const {Builder, By, Key, until } = require("selenium-webdriver");
 const {expect} = require('chai');
 
-describe ('Site ChromeDrives test', function () {
+describe ('Site ChromeDrivers test', function () {
     let driver;
     before (async ()=> {
         driver = await new Builder().forBrowser('chrome').build();
-        await driver.manage().window().maximize();  
+        await driver.manage().window().maximize(); 
+        await driver.manage().setTimeouts( { implicit: 10000 } ); 
     });
 
      after (async () => {
@@ -13,49 +14,40 @@ describe ('Site ChromeDrives test', function () {
      }); 
 
 
-it ('1. Title contains ChromeDriver', async () => {
+it ('1. Title should contains ChromeDriver', async () => {
     
     await driver.get('https://chromedriver.chromium.org/home');
     const compareTitle = await driver.getTitle();
     expect (await compareTitle).to.contain('ChromeDriver')
 })
 
-it ('2. Highlighting Chrome Extensions', async () => {
+it ('2. Title should contains "Chrome Extensions" and highlighting ', async () => {
     await driver.get ('https://chromedriver.chromium.org/home');
-    const buttonChromeExtension = await driver.findElement(By.xpath(`//*[@class ='VsJjtf'][3]`));
-    await buttonChromeExtension.click();
-    //const el = await driver.findElement(By.xpath(//*[@class='K1Ci7d oXBWEc jYxBte']));  не срабатывает
-    //driver.executeScript("arguments[0].style.backgroundColor = '" + "red" + "'", el)
+    await driver.findElement(By.xpath(`//*[@class ='VsJjtf'][3]`)).click();
+    const h1HighLight = await driver.findElement(By.xpath(`//h1['Rn3Z1b']`));
+    driver.executeScript("arguments[0].style.backgroundColor = '" + "red" + "'", h1HighLight); // к сожалению, не смог подсветить title, сделал подсветку для h1
     const compareTitle = await driver.getTitle();
     expect (await compareTitle).to.contain('Chrome Extensions');
-    await driver.sleep(3000);
+    
 }) 
 
-it ('3. The search field should contain "driver"', async () => {
+it ('3. The search field should contains "driver"', async () => {
     await driver.get('https://chromedriver.chromium.org/home');
-    const searchButton = await driver.findElement(By.xpath(`//*[@class= 'vu8Pwe tCHXDc YSH9J']`));
-    await searchButton.click();
-    await driver.findElement(By.xpath(`//*[@class= 'whsOnd zHQkBf']`)).sendKeys('driver', Key.ENTER);
-    await driver.sleep(3000);
+    await driver.findElement(By.xpath(`//*[@class= 'vu8Pwe tCHXDc YSH9J']`)).click();
+    await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath(`//*[@class= 'whsOnd zHQkBf']`))),2000).sendKeys('driver', Key.ENTER);
     const searchField = await driver.findElement(By.xpath(`//*[@class ='gtazFe'][1]`))
-    expect (await searchField.getText()).to.contain('driver');    //.toLowerCase() с ним не работает
-    driver.executeScript("arguments[0].style.backgroundColor = '" + "red" + "'", searchField)  // убрать, пример для предыдущего теста
-    await driver.sleep(3000);  
+    await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath(`//*[@class ='gtazFe'][1]`))),2000);
+    expect (await searchField.getText()).to.contain('driver');    //.toLowerCase() ---- с ним не работает
+    
 })
 
-it ('URL should be contains "/mobile-emulation"', async () => {
+it ('URL should contains "/mobile-emulation"', async () => {
     await driver.get('https://chromedriver.chromium.org/home');
-    const additionallyButton = await driver.findElement(By.xpath(`//a[text()='Дополнительно']`));
-    additionallyButton.click();
-    const emulationButton = await driver.findElement(By.xpath(`//div[@class='PsKE7e IKA38e oNsfjf']//a[text()='Mobile Emulation']`));
-    emulationButton.click();
-    await driver.sleep(1000); 
-    //await driver.wait(until.elementIsVisible(emulation));
+    await driver.findElement(By.xpath(`//a[text()='Дополнительно']`)).click();
+    await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath(`//div[@class='PsKE7e IKA38e oNsfjf']//a[text()='Mobile Emulation']`)))).click();
     const compareUrl = await driver.getCurrentUrl();    
     expect (await compareUrl).to.contain('/mobile-emulation');
     
 })
 
 })
-
-
